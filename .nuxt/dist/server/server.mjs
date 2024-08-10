e4bb4f7cbe17b9e699c912e5f54ef8972066d61d
bennyekop <bennyster666@gmail.com>
2024-08-10 19:27:15 +0700

    commit 2

--- .nuxt/dist/server/server.mjs
+++
@@ -1,1 +0,0 @@
-export { default } from "file:///C:/Users/Master/Documents/benny/nuxt-cp/nuxt-01/node_modules/@nuxt/vite-builder/dist/runtime/vite-node.mjs"
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  modules: [
+    "nuxt-icon",
+    "@nuxt/image",
+    "@nuxtjs/tailwindcss",
+    "nuxt-headlessui",
+  ],
+  tailwindcss: {
+    cssPath: "~/assets/css/tailwind.css",
+    config: {
+      theme: {
+        fontFamily: {
+          'poppins': ['Poppins', 'sans-serif'],
+          'volkhov': ['Volkhov', 'serif']
+        }
+      },
+      plugins: [
+        require('tailwind-scrollbar-hide')
+      ]
+    }
+  },
+});
